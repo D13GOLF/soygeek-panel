944e0986deff746902cd13fdb7b94eb0d64c372c
D13GOLF <63372181+D13GOLF@users.noreply.github.com>
2025-04-21 05:52:22 -0400

    Update dashboard.py

--- core/dashboard.py
+++ core/dashboard.py
@@ -5,7 +5,7 @@ dashboard_bp = Blueprint('dashboard', __name__)
 
 @dashboard_bp.route('/')
 def home():
-    return render_template('dashboard/index.html', title="Panel de Control")
+    return render_template('dashboard.html', title="Panel de Control")
 
 @dashboard_bp.route('/clientes')
 def clientes():
